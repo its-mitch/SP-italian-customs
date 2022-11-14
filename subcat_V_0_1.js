60a65f5c393fa9c42cf9784724169a91082c67eb
its-mitch <mircea.gida@gmail.com>
2022-11-14 16:22:00 +0100

    fix sub fillter

--- subcat_V_0_1.js
+++ subcat_V_0_1.js
@@ -10,10 +10,10 @@
             const [firstItem] = listInstance.items;
             listInstance.clearItems();
             const itemTemplateElement = firstItem.element;
-            
+
             const products = await fetchProducts();
             //const storProd=window.localStorage.getItem("storedProducts");
-            
+
             const newItems = products.map((product) => createItem(product, itemTemplateElement));
             await listInstance.addItems(newItems);
             //window.fsAttributes.cmsfilter.init();
@@ -41,8 +41,8 @@
      * Fetches fake products from Fake Store API.
      * @returns An array of {@link Product}.
      */
-    
-     const fetchProducts = async () => {
+
+    const fetchProducts = async () => {
         try {
             const response = await fetch('https://data.mongodb-api.com/app/sp-gaodj/endpoint/sp_shop_api');
             const data = await response.json();
@@ -66,135 +66,138 @@
      */
     const createItem = (product, templateElement) => {
 
-        var ref=window.location.href;
-        var sottocatUrl="";
-    
-        if (ref.includes("sottocategorie")){
-          var x=ref.split("/");
-          sottocatUrl=x[x.length-1].replace("-"," ");
+        var ref = window.location.href;
+        var sottocatUrl = "";
+
+        if (ref.includes("sottocategorie")) {
+            var x = ref.split("/");
+            sottocatUrl = x[x.length - 1].replace("-", " ");
         }
-    
 
-        // Clone the template element
-        const newItem = templateElement.cloneNode(true);
+        if (product.subcategory = sottocatUrl) {
 
-        // Query inner elements
-        const image = newItem.querySelector('[data-element="image"]');
-        const name = newItem.querySelector('[data-element="name"]');
-        const category = newItem.querySelector('[data-element="category"]');
-        const subcategory = newItem.querySelector('[data-element="subcategory"]');
-        const make = newItem.querySelector('[data-element="make"]');
-        const model = newItem.querySelector('[data-element="model"]');
-        const producer = newItem.querySelector('[data-element="producer"]');
-        const button = newItem.querySelector('[data-element="link"]');
-        const price = newItem.querySelector('[data-element="price"]');
-        const year = newItem.querySelector('[data-element="year"]');
+            // Clone the template element
+            const newItem = templateElement.cloneNode(true);
+
+            // Query inner elements
+            const image = newItem.querySelector('[data-element="image"]');
+            const name = newItem.querySelector('[data-element="name"]');
+            const category = newItem.querySelector('[data-element="category"]');
+            const subcategory = newItem.querySelector('[data-element="subcategory"]');
+            const make = newItem.querySelector('[data-element="make"]');
+            const model = newItem.querySelector('[data-element="model"]');
+            const producer = newItem.querySelector('[data-element="producer"]');
+            const button = newItem.querySelector('[data-element="link"]');
+            const price = newItem.querySelector('[data-element="price"]');
+            const year = newItem.querySelector('[data-element="year"]');
 
 
-        // Populate inner elements
-        var defaultImage =
-            "https://uploads-ssl.webflow.com/61e6e776f7b79f4f941b254e/61eb5f843eec5928ee20796b_logo_slate_grey.svg";
+            // Populate inner elements
+            var defaultImage =
+                "https://uploads-ssl.webflow.com/61e6e776f7b79f4f941b254e/61eb5f843eec5928ee20796b_logo_slate_grey.svg";
 
-        if (product.img != null) {
-            if (image) image.src = product.img;
-        } else {
-            if (image) image.src = defaultImage;
-        }
-        if (name) name.textContent = product.name;
-        if (category) category.textContent = product.category;
-        if (subcategory) subcategory.textContent = product.subcategory;
-
-        //important
-        if (product.Comp != null) {
-            let yearSequence = [];
-            let makeSequence = [];
-            let modelSequence = [];
-            let compArray = [];
-            if (Array.isArray(product.Comp)) {
-                compArray = product.Comp;
+            if (product.img != null) {
+                if (image) image.src = product.img;
             } else {
-                compArray = product.Comp.split(" <> ");
+                if (image) image.src = defaultImage;
             }
-            var i = 0;
-            while (i < compArray.length) {
-                let compParts = compArray[i].split(" ");
-                let compYears = compParts[compParts.length - 1];
-                let charCount = compYears.split("");
-                let yearStart = 0;
-                let yearEnd = 0;
-                if (charCount.length > 4) {
-                    yearStart = compYears.slice(0, 4);
-                    yearEnd = compYears.slice(4);
+            if (name) name.textContent = product.name;
+            if (category) category.textContent = product.category;
+            if (subcategory) subcategory.textContent = product.subcategory;
+
+            //important
+            if (product.Comp != null) {
+                let yearSequence = [];
+                let makeSequence = [];
+                let modelSequence = [];
+                let compArray = [];
+                if (Array.isArray(product.Comp)) {
+                    compArray = product.Comp;
                 } else {
-                    yearStart = compYears;
-                    yearEnd = 2023;
+                    compArray = product.Comp.split(" <> ");
                 }
-                let yearIndex = 0;
-                while (yearStart < yearEnd) {
-                    yearSequence[yearIndex] = yearStart;
-                    yearIndex++;
-                    yearStart++;
-                }
-                makeSequence[i] = compParts[0];
-                let j = 1;
-                while (j < compParts.length - 1) {
-                    if (modelSequence[i] != undefined) {
-                        modelSequence[i] = modelSequence[i] + " " + compParts[j];
+                var i = 0;
+                while (i < compArray.length) {
+                    let compParts = compArray[i].split(" ");
+                    let compYears = compParts[compParts.length - 1];
+                    let charCount = compYears.split("");
+                    let yearStart = 0;
+                    let yearEnd = 0;
+                    if (charCount.length > 4) {
+                        yearStart = compYears.slice(0, 4);
+                        yearEnd = compYears.slice(4);
                     } else {
-                        modelSequence[i] = compParts[j];
+                        yearStart = compYears;
+                        yearEnd = 2023;
                     }
-                    j++;
+                    let yearIndex = 0;
+                    while (yearStart < yearEnd) {
+                        yearSequence[yearIndex] = yearStart;
+                        yearIndex++;
+                        yearStart++;
+                    }
+                    makeSequence[i] = compParts[0];
+                    let j = 1;
+                    while (j < compParts.length - 1) {
+                        if (modelSequence[i] != undefined) {
+                            modelSequence[i] = modelSequence[i] + " " + compParts[j];
+                        } else {
+                            modelSequence[i] = compParts[j];
+                        }
+                        j++;
+                    }
+                    i++;
                 }
-                i++;
-            }
-            make.textContent = makeSequence;
-            if (make) make.textContent = makeSequence;
-            model.textContent = modelSequence;
-            if (model) model.textContent = modelSequence;
-            year.textContent = yearSequence;
-            if (year) year.textContent = yearSequence;
-        } else {
-            if (product.make != null) {
-                if (make) make.textContent = product.make.toString().split(",").filter(onlyUnique);
+                make.textContent = makeSequence;
+                if (make) make.textContent = makeSequence;
+                model.textContent = modelSequence;
+                if (model) model.textContent = modelSequence;
+                year.textContent = yearSequence;
+                if (year) year.textContent = yearSequence;
             } else {
-                if (make) make.textContent = "";
-            }
-            if (product.model != null) {
-                if (model) model.textContent = product.model.toString().split(",").filter(onlyUnique);
-            } else {
-                if (model) model.textContent = "";
-            }
-            if (product.year != null) {
-                if (product.year != "Universale") {
-                    if (Array.isArray(product.year)) {
-                        var yearArray = product.year;
-                        yearArray = yearArray.filter(onlyUnique);
-                        yearArray = yearArray.sort();
-                        if (year) year.textContent = yearArray;
-                        years = product.year.filter(onlyUnique);
+                if (product.make != null) {
+                    if (make) make.textContent = product.make.toString().split(",").filter(onlyUnique);
+                } else {
+                    if (make) make.textContent = "";
+                }
+                if (product.model != null) {
+                    if (model) model.textContent = product.model.toString().split(",").filter(onlyUnique);
+                } else {
+                    if (model) model.textContent = "";
+                }
+                if (product.year != null) {
+                    if (product.year != "Universale") {
+                        if (Array.isArray(product.year)) {
+                            var yearArray = product.year;
+                            yearArray = yearArray.filter(onlyUnique);
+                            yearArray = yearArray.sort();
+                            if (year) year.textContent = yearArray;
+                            years = product.year.filter(onlyUnique);
+                        } else {
+                            if (year) year.textContent = product.year;
+                        }
                     } else {
                         if (year) year.textContent = product.year;
                     }
                 } else {
-                    if (year) year.textContent = product.year;
+                    year.textContent[0] = "Universale";
+                    years = "Universale";
                 }
-            } else {
-                year.textContent[0] = "Universale";
-                years = "Universale";
             }
+            if (producer) producer.textContent = product.Brand;
+            if (button) button.setAttribute('href', 'https://sp-customs-2.webflow.io/articoli?id=' + product._id);
+            //if (code) code.textContent = product.code;
+            //if (year) year.textContent = product.year;
+
+            let priceArr = product.price.toString();
+            priceArr = priceArr.split(",").filter(onlyUnique);
+            priceArr = priceArr.sort();
+            if (price) price.textContent = "€" + addZeroes(priceArr[0]);
+
+            return newItem;
+        }else{
+            return null;
         }
-        if (producer) producer.textContent = product.Brand;
-        if (button) button.setAttribute('href', 'https://sp-customs-2.webflow.io/articoli?id=' + product._id);
-        //if (code) code.textContent = product.code;
-        //if (year) year.textContent = product.year;
-
-        let priceArr = product.price.toString();
-        priceArr = priceArr.split(",").filter(onlyUnique);
-        priceArr = priceArr.sort();
-        if (price) price.textContent = "€" + addZeroes(priceArr[0]);
-
-
-        return newItem;
     };
 
     function onlyUnique(value, index, self) {
