c2e7f5c4e203084fe4dd79b6ca0a8815f5a4b211
its-mitch <mircea.gida@gmail.com>
2022-12-02 20:29:25 +0100

    fix for

--- Articoli_V_11_1.js
+++ Articoli_V_11_1.js
@@ -344,7 +344,7 @@ var globalCode;
     if (product.spec != null) {
       description.textContent += "\r\nSPECIFICHE VARIANTI\r\n";
       if (Array.isArray(product.spec)) {
-        for (let n = 0; n < product.length; n++) {
+        for (let n = 0; n < product.spec.length; n++) {
           description.textContent += product.spec[n];
         }
       }
